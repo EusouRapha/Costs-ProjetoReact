9f550895d6192045154ba274fe55dd793199e560
Raphael Gomes <raphaelgw@gmail.com>
2023-04-12 16:51:00 -0300

    Inserindo dados no banco via API

--- projeto/src/components/project/ProjectForm.js
+++ projeto/src/components/project/ProjectForm.js
@@ -1,46 +1,72 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./ProjectForm.module.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
-useEffect(()=>{
-  fetch("http://localhost:5000/categories", {
-    //REQUEST COM O FETCHAPI PARA A URL
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((resp) => resp.json()) //pegou o dado e transformou em json
-    .then((data) => {
-      setCategories(data); //pegou os dados em json e colocou no hook setcategories
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      //REQUEST COM O FETCHAPI PARA A URL
+      method: "GET", //GET BUSCA DADOS
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .catch((err) => console.log(err));
-},[])
+      .then((resp) => resp.json()) //pegou o dado e transformou em json
+      .then((data) => {
+        setCategories(data); //pegou os dados em json e colocou no hook setcategories
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const submit = (e) => {
+    e.preventDefault();
+    // console.log(project)
+    handleSubmit(project);
+  };
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ''}
       ></Input>
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ''}
       ></Input>
       <Select
         name="category_id"
         text="Selecione a categoria"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ''}
       ></Select>
       <SubmitButton text={btnText}></SubmitButton>
     </form>
