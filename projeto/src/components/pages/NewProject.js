eb598e7184a96d2687715b15d7eaa52be1bb784a
Raphael Gomes <raphaelgw@gmail.com>
2023-04-13 15:14:40 -0300

    Resgatando projetos do banco de dados

--- projeto/src/components/pages/NewProject.js
+++ projeto/src/components/pages/NewProject.js
@@ -20,7 +20,7 @@ function NewProject(){
         }).then((resp => resp.json()))
         .then((data => {
             console.log(data)
-            navigate('/projects',{message: 'Projeto criado com sucesso!'}) //redireciona
+            navigate('/projects', { state: {message: 'Projeto criado com sucesso!'} }) //redireciona
         }))
         .catch((err) => console.log(err));
 
--- projeto/src/components/pages/Projects.js
+++ projeto/src/components/pages/Projects.js
@@ -1,6 +1,61 @@
-import React from "react"
-function Projects(){
-    return <h1>Projects</h1>
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Message from "../layout/Message";
+import styles from "./Projects.module.css";
+import Container from "../layout/Container";
+import Loading from "../layout/Loading";
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading ] = useState(false)
+
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    setTimeout(()=>{
+        fetch("http://localhost:5000/projects", {
+            method: "GET", //RECEBE DADOS
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((resp) => resp.json())
+            .then((data) => {
+              console.log(data);
+              setProjects(data);
+              setRemoveLoading(true);
+            })
+            .catch((err) => console.log(err));
+    },1500)
+  }, []);
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"></LinkButton>
+      </div>
+      {message && <Message type="sucess" msg={message}></Message>}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard 
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.key}
+            ></ProjectCard>
+          ))}
+          {!removeLoading && <Loading/>}
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
